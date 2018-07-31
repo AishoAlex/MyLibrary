6d2c500b191dcc6173fea2fec386053e99622188
Aisho Alex <shinja@vivaldi.net>
2018-07-31 23:01:20 +0300

    Getting site data. Start build html parser

--- HtmlParser.java
+++ HtmlParser.java
@@ -1,9 +1,30 @@
-public class HtmlParser {
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class HtmlParser {
+    private StringBuffer html;
     public int HtmlParser(StringBuffer html){
        if(html == null){
            return -1;
        }
+       this.html = new StringBuffer(html);
        return 0;
+    }
+    public StringBuffer getTag(String tagName){
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            //TODO add handler, read documentation...
+        }
+        catch (SAXException e){
+            System.out.println(e);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        StringBuffer output = new StringBuffer();
+        return output;
     }
 }
